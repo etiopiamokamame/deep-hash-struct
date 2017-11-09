d5b8e5066f00e5be7638e83e5f5129f917350254
adachi <int_haimaki.mt32@ezweb.ne.jp>
2017-11-09 21:15:17 +0900

    issues/2 Rowクラスにside_or_header?メソッド追加

--- spec/deep/hash/struct/dashboard/table/row_spec.rb
+++ spec/deep/hash/struct/dashboard/table/row_spec.rb
@@ -83,4 +83,44 @@ describe Deep::Hash::Struct::Dashboard::Table::Row do
       end
     end
   end
+
+  describe "#side_or_header?" do
+    context "When there is a header and side option" do
+      before do
+        options[:header] = true
+        options[:side]   = true
+      end
+
+      it "Return true" do
+        expect(row.side_or_header?).to be_truthy
+      end
+    end
+
+    context "When there is a header option" do
+      before do
+        options[:header] = true
+      end
+
+      it "Return true" do
+        expect(row.side_or_header?).to be_truthy
+      end
+    end
+
+    context "When there is a side option" do
+      before do
+        options[:side] = true
+      end
+
+      it "Return true" do
+        expect(row.side_or_header?).to be_truthy
+      end
+    end
+
+
+    context "When there is no header and side option" do
+      it "Return false" do
+        expect(row.side_or_header?).to be_falsey
+      end
+    end
+  end
 end
